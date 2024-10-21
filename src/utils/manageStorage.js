936ca89311d9be47ce77519cd992b293384dc364
Bartosz Czarnecki <bartosz.czarnecki.career@gmail.com>
2024-10-21 12:35:01 +0200

    Updates

--- src/utils/manageStorage.js
+++
@@ -1,8 +0,0 @@
-export function save(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-
-export function download(key) {
-  const savedData = localStorage.getItem(key);
-  return JSON.parse(savedData);
-}
